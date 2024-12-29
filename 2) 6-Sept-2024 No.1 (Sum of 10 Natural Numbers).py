545fb7a5cee84d0cc15681e3b8aec64ba88d92eb
Ujjwal Jaiswal <anshuujjwaljaiswal@gmail.com>
2024-12-29 14:47:58 +0530

    Add files via upload

---
+++ 2) 6-Sept-2024 No.1 (Sum of 10 Natural Numbers).py
@@ -0,0 +1,10 @@
+# Ujjwal Jaiswal CSC/24/56
+# Write a program which will find the sum of first 10 natural numbers.
+
+print("Finding the sum of first 10 natural numbers")
+num=1
+sum=0
+while num<=10:
+    sum+=num
+    num+=1
+print(sum)
